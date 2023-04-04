d21a2b59f323ad2bd2437433621f6983bad56521
Barbara Laurince <blaurince@hotmail.com>
2023-04-04 16:10:50 -0400

    ADD endpoint and imported tvLibrary

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 import express from 'express';
 import cors from 'cors';
 
+import { addNewTv, getAllTv } from './src/tvLibrary.js';
+
+
+
 const PORT = 3000;
 
 const app = express();
 app.use( cors() );
 app.use( express.json() );
+
+app.get("/tvShows", getAllTv );
+
+app.post("/tvShows", addNewTv);
 
 app.listen(PORT, () => {
    console.log(`Listening on http://localhost:${PORT}`);
